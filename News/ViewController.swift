cb1c74fdbfc0eb12c2ecd8b75168c3d805c8fc01
HFZ321 <hongfei1106@gmail.com>
2021-10-15 10:39:59 -0400

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Hongfei Zheng on 10/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
